9f48a46e9f54b793c2c827722d0a1f668a51b319
julianabot <julianarolluqui@gmail.com>
2021-11-24 15:14:03 +0800

    Fonts, Color Constants, and Test

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const kMainDarkGreen = Color(0xFF57706F);
+const kVeryDarkGreen = Color(0xFF2B3E3C);
+const kGold = Color(0xFFDFC3A2);
+const kOffWhite = Color(0xFFF4EDE5);
+
+const kRed = Color(0xFFCE9F90);
+const kBlue = Color(0xFFA1BFC9);
+const kGreen = Color(0xFF9FCC90);
+const kPurple = Color(0xFFC49CD7);
+
+const kGrayTextField = Color(0xFFE6E3DF);
+const kGrayButton = Color(0xFFADADAD);
